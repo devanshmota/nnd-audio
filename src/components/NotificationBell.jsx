31cbf3c7f2e23fee98b016bef8ed2abd94188f3f
devanshmota <devanshmota@gmail.com>
2024-02-23 11:58:13 +0530

    push notificatipn packege removed

--- src/components/NotificationBell.jsx
+++ src/components/NotificationBell.jsx
@@ -1,5 +1,4 @@
 'use client'
-import addNotification from 'react-push-notification';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import { Badge } from '@mui/material';
 import NotificationCanvas from './NotificationCanvas';
@@ -29,19 +28,9 @@ const NotificationBell = () => {
         setIsOffCanvasOpen(true)
     }
 
-    // const addNotifications = () => {
-    //     addNotification({
-    //         title: 'dddddddddd',
-    //         message: 'aaa',
-    //         duration: 4000,
-    //         icon: '/images/nnd_logo.png',
-    //         native: true
-    //     })
-    // }
 
     return (
         <>
-            {/* <button onClick={addNotifications}>click</button> */}
             <Badge badgeContent={notifications.length} color="primary" className='ntfc_badge' onClick={handleOpen} >
                 <NotificationsIcon />
             </Badge>
